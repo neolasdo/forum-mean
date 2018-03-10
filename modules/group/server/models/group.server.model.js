4527f662eeff5840dbcad97380dd1db673b4d747
tuannd <tuannd@gmail.com>
2018-03-10 15:03:07 +0700

    update sidebar

---
+++ modules/group/server/models/group.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Group Schema
+ */
+var GroupSchema = new Schema({
+  // Group model fields
+  // ...
+});
+
+mongoose.model('Group', GroupSchema);
