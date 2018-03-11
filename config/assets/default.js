53aaf649479f9ef2f0de64f8a4782c7da5ec35b8
tuannd <tuannd@gmail.com>
2018-03-11 18:30:37 +0700

    create add class modal

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,8 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/*/dist/*.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -16,7 +17,10 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/*/dist/*.js',
+        'public/lib/opentok-layout-js/opentok-layout.js',
+        'public/lib/opentok-angular/opentok-angular.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
