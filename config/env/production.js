47f9429cb9b4c147baf5fc29966742c69fd92f96
tuannguyen <tuannd@gemvietnam.com>
2018-03-13 16:30:07 +0700

    production env mongo

--- config/env/production.js
+++ config/env/production.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   port: process.env.PORT || 8443,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI|| 'mongodb://tuannd995:Tuannguyen1995@@ds111279.mlab.com:11279/forum-mean' || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI|| 'mongodb://tuannd995:abcdefgh@ds111279.mlab.com:11279/forum-mean' || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
       user: '',
       pass: ''
