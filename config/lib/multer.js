a097b8ba31b3a05c5a6e0455a49da238ded6411b
Tuan <tuannd@gemvietnam.com>
2018-04-17 17:58:34 +0700

    set multer

--- config/lib/multer.js
+++ config/lib/multer.js
@@ -6,3 +6,17 @@ module.exports.profileUploadFileFilter = function (req, file, cb) {
   }
   cb(null, true);
 };
+
+module.exports.documentUploadFileFilter = function (req, file, cb) {
+  if (file.mimetype !== 'application/msword'
+      && file.mimetype !== 'application/json'
+      && file.mimetype !== 'application/vnd.ms-powerpoint'
+      && file.mimetype !== 'application/pdf'
+      && file.mimetype !== 'application/vnd.ms-excel'
+      && file.mimetype !== 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+      && file.mimetype !== 'text/csv'
+      && file.mimetype !== 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+    return cb(new Error('Only documents files are allowed!'), false);
+  }
+  cb(null, true);
+};
